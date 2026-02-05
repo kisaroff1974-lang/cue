5e2e5c3dc0d5c56fb903b38f004306f439b49a17
kisaroff1974-lang <kisaroff1974@gmail.com>
2026-02-06 01:54:16 +0300

    Create App.tsx

---
+++ src/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div style={{ padding: 40, fontSize: 24 }}>
+      🎭 Подай реплику  
+      <br />
+      Проект загружен и работает.
+    </div>
+  )
+}
